bc79b6d69db4151bc291a49a52f47612a7813965
Mike Manfrin <mike.manfrin@therealreal.com>
2014-10-15 14:02:52 -0700

    BigDecimal usage instead of floats in spree.

--- core/app/models/spree/calculator/flat_percent_item_total.rb
+++ core/app/models/spree/calculator/flat_percent_item_total.rb
@@ -14,7 +14,7 @@ module Spree
       return unless object.present? and object.line_items.present?
       item_total = object.line_items.map(&:amount).sum
       value = item_total * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
-      (value * 100).round.to_f / 100
+      value
     end
   end
 end
--- core/app/models/spree/calculator/flexi_rate.rb
+++ core/app/models/spree/calculator/flexi_rate.rb
@@ -17,15 +17,15 @@ module Spree
     end
 
     def compute(object)
-      sum = 0
+      sum = BigDecimal.new('0')
       max = self.preferred_max_items.to_i
       items_count = object.line_items.map(&:quantity).sum
       items_count.times do |i|
         # check max value to avoid divide by 0 errors
         if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
-          sum += self.preferred_first_item.to_f
+          sum += self.preferred_first_item
         else
-          sum += self.preferred_additional_item.to_f
+          sum += self.preferred_additional_item
         end
       end
 
