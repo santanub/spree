aad31880febf896f4cbdcaa0ec6f03764fcacb3a
Mike Manfrin <mike.manfrin@therealreal.com>
2014-10-21 16:54:53 -0700

    Better BDs.

--- core/app/models/spree/calculator/flat_percent_item_total.rb
+++ core/app/models/spree/calculator/flat_percent_item_total.rb
@@ -13,8 +13,8 @@ module Spree
     def compute(object)
       return unless object.present? and object.line_items.present?
       item_total = object.line_items.map(&:amount).sum
-      value = item_total * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
-      value
+      value = item_total * BigDecimal.new(self.preferred_flat_percent.to_s) / BigDecimal.new('100')
+      value.round(2)
     end
   end
 end
